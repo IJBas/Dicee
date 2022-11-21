3ee28d0a42c3d37b121beeef6353b36e9ee2ba2d
IJBas <113275082+IJBas@users.noreply.github.com>
2022-11-21 15:44:22 +0800

    Add files via upload

---
+++ diceee.js
@@ -0,0 +1,18 @@
+const randomOne = Math.floor(Math.random() * 6) + 1;
+const randomTwo = Math.floor(Math.random() * 6) + 1;
+
+const diceImgOne = 'images/dice' + randomOne + '.png';
+const diceImgTwo = 'images/dice' + randomTwo + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src',diceImgOne);
+document.querySelectorAll('img')[1].setAttribute('src',diceImgTwo);
+
+if (randomOne > randomTwo){
+  document.querySelector('h1').innerHTML = "Player 1 WINS 🚩";
+}
+else if (randomTwo > randomOne){
+  document.querySelector('h1').innerHTML = "Player 2 WINS 🚩🚩";
+}
+else{
+  document.querySelector('h1').innerHTML = "Draw 😱";
+}
